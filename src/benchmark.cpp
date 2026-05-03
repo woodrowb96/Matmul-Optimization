acf821f5c779dc6881ae5a96058f6761eb520e03
woodrowb <brenwoodr96@gmail.com>
2026-05-02 18:48:00 -0700

    fix: time_matmul now re-zeros the result matrix C between runs
    the matmul functions assume C is all 0 when its passed in, if C is not 0 the result of the matmul is incorect

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -6,15 +6,22 @@
 double time_matmul(std::function<void(const Matrix&, const Matrix&, Matrix&)> matmul,
                     const Matrix& A, const Matrix& B, Matrix& C, int runs)
 {
+  double total_duration {0};
+
+  C.zero();       //make sure C is all 0s, so the dot_prods are correct
   matmul(A,B,C);  //run once to warmup the cache
 
-  auto start = std::chrono::steady_clock::now();
   for(int i = 0; i < runs; i++) {
-    matmul(A,B,C);
-  }
-  auto end = std::chrono::steady_clock::now();
+    C.zero(); //make sure C is all 0's before each run
 
-  double avg_duration = std::chrono::duration<double>(end - start).count() / runs;
+    auto start = std::chrono::steady_clock::now();
+    matmul(A,B,C);
+    auto end = std::chrono::steady_clock::now();
+
+    total_duration += std::chrono::duration<double>(end - start).count();
+  }
+
+  double avg_duration = total_duration / runs;
 
   return avg_duration;
 }
