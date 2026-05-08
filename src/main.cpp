d8baaf907b7b06c866f9f5548e36aa80d2efcd92
woodrowb <brenwoodr96@gmail.com>
2026-05-07 22:36:39 -0700

    feat: wire matmul_tiled_threaded into main benchmark

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,40 @@
 #include "matrix.h"
 #include "matmul_naive.h"
 #include "matmul_tiled_cpu.h"
+#include "matmul_tiled_threaded.h"
 #include "benchmark.h"
 
 #include <iostream>
+#include <thread>
 
 int main()
 {
-  Matrix a = Matrix::identity(5);
-  a.print();
+  int tile_i = 32, tile_j = 32, tile_k = 32;
+  auto tiled_call = [tile_i, tile_j, tile_k](const Matrix& A, const Matrix& B, Matrix& C) {
+    matmul_tiled_cpu(A, B, C, tile_i, tile_j, tile_k);
+  };
 
-  Matrix b = Matrix::zeros(5,3);
-  b.print();
-  
-  
-  // int tile_i = 32, tile_j = 32, tile_k = 32;
-  // auto tiled_call = [tile_i, tile_j, tile_k](const Matrix& A, const Matrix& B, Matrix& C) {
-  //   matmul_tiled_cpu(A, B, C, tile_i, tile_j, tile_k);
-  // };
-  //
-  // Matrix a = Matrix::random(5,5,42);
-  // Matrix b = Matrix::random(5,7,100);
-  // Matrix c (5,7);
-  // TimingStats stats = time_matmul(matmul_naive, a, b, c, 5);
-  // print_times("(5x5) X (5x7) (naive)", stats);
-  //
-  // c.zero();
-  // stats = time_matmul(tiled_call, a, b, c, 5);
-  // print_times("(5x5) X (5x7) (tiled_cpu)", stats);
-  //
-  //
+  int num_threads = std::thread::hardware_concurrency();
+  if(num_threads == 0) {
+    std::cout << "std::thread::hardware_concurency() returning 0, falling back to 4 threads\n";
+    num_threads = 4;
+  }
+  auto threaded_call = [tile_i, tile_j, tile_k, num_threads](const Matrix& A, const Matrix& B, Matrix& C) {
+    matmul_tiled_threaded(A, B, C, tile_i, tile_j, tile_k, num_threads);
+  };
+
+  Matrix a = Matrix::random(5,5,42);
+  Matrix b = Matrix::random(5,7,100);
+  Matrix c = Matrix::zeros(5,7);
+  TimingStats stats = time_matmul(matmul_naive, a, b, c, 5);
+  print_times("(5x5) X (5x7) (naive)", stats);
+  c.zero();
+  stats = time_matmul(tiled_call, a, b, c, 5);
+  print_times("(5x5) X (5x7) (tiled_cpu)", stats);
+  c.zero();
+  stats = time_matmul(threaded_call, a, b, c, 5);
+  print_times("(5x5) X (5x7) (threaded_cpu)", stats);
+
   // Matrix d = Matrix::random(100, 500, 47);
   // Matrix e = Matrix::random(500, 100, 91);
   // Matrix f (100, 100);
